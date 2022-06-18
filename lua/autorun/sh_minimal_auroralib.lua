f61d5bf6a9f52350351e57353d86c542b9796891
Dr4iv3rNope <dr4iv3r2004@gmail.com>
2022-06-19 01:18:03 +0300

    Update sh_minimal_auroralib.lua

--- lua/autorun/sh_minimal_auroralib.lua
+++ lua/autorun/sh_minimal_auroralib.lua
@@ -1,5 +1,12 @@
 if auroralib and not auroralib._MINIMAL then return end
 
+local rawget = rawget
+local rawset = rawset
+local select = select
+local table_insert = table.insert
+local table_remove = table.remove
+local auroralib_Assert = auroralib.Assert
+
 auroralib = auroralib or {}
 auroralib._MINIMAL = true
 
@@ -72,4 +79,110 @@ end
 
 function ENTITY:TimeoutIsExpired(id)
 	return CurTime() > self:TimeoutGetUntil(id)
+end
+
+local SET = {}
+
+function SET.__index(tbl, k)
+	return rawget(SET, k) or rawget(tbl, "values")[k]
+end
+
+function SET.__newindex(tbl, index, value)
+	local values = rawget(tbl, "values")
+
+	if values[index] == nil then
+		SET.insert(tbl, value)
+
+		return
+	end
+
+	rawset(values, index, value)
+end
+
+function SET:length()
+	return #rawget(self, "values")
+end
+
+function SET:has(key)
+	return rawget(self, "keys")[key] ~= nil
+end
+
+function SET:insert(...)
+	local keys, values = rawget(self, "keys"), rawget(self, "values")
+	local length = #values
+	local pos, value
+
+	if select("#", ...) == 1 then
+		pos = length + 1
+		value = select(1, ...)
+	else
+		pos = select(1, ...)
+		value = select(2, ...)
+	end
+
+	auroralib_Assert(value ~= nil, "value cannot be nil")
+
+	local index = keys[value]
+
+	if index ~= nil then
+		if index ~= pos then
+			-- if index is different, move value
+
+			self:remove(index)
+		else
+			-- if insert index is same, override value
+			values[index] = value
+
+			return pos
+		end
+	end
+
+	keys[value] = table_insert(values, pos, value)
+
+	self:reconstruct(pos + 1)
+
+	return pos
+end
+
+function SET:remove(pos)
+	local keys, values = rawget(self, "keys"), rawget(self, "values")
+
+	local value = values[pos]
+	if value == nil then return end
+
+	table_remove(values, pos)
+	keys[value] = nil
+
+	self:reconstruct(pos)
+
+	return value
+end
+
+function SET:delete(key)
+	local keys, values = rawget(self, "keys"), rawget(self, "values")
+
+	local index = keys[key]
+	if index == nil then return end
+
+	table_remove(values, index)
+	keys[key] = nil
+
+	self:reconstruct(index)
+end
+
+function SET:reconstruct(from)
+	local keys, values = rawget(self, "keys"), rawget(self, "values")
+
+	for i = from, #values do
+		local v = values[i]
+
+		keys[v] = i
+	end
+end
+
+function auroralib.Set()
+	return setmetatable({
+		keys = {},
+		values = {}
+	}, SET)
 end
